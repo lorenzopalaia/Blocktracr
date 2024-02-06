48f6c37655413f71a871c26338790e2eaf210587
Lorenzo Palaia <lorenzopalaia53@gmail.com>
2024-02-06 18:47:42 +0100

    vercel deployment

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import CoinBar from './components/CoinBar.vue'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+
 </script>
 
 <style scoped>
