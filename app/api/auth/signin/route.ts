12731258f5c43795bdc46d2eaf1db4e48873c4dd
lorenzopalaia <lorenzopalaia53@gmail.com>
2025-02-26 20:24:27 +0100

    Redirect to account on signin

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: NextRequest) {
   }
 
   revalidatePath("/", "layout");
-  return NextResponse.redirect(new URL("/dashboard", req.url), {
+  return NextResponse.redirect(new URL("/account", req.url), {
     status: 302,
   });
 }
