51340bdfcbb9fe4a2429f9c9ba3d59d05d4c0edf
lorenzopalaia <lorenzopalaia53@gmail.com>
2025-10-22 10:13:39 +0200

    chore: update dependencies (Nextjs 16)

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // update user's auth session
   return await updateSession(request);
 }
